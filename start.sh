6e0ae9c20ca62e38490e238d252966e7eb4191b0
otobit-sahil <132433890+otobit-sahil@users.noreply.github.com>
2023-06-11 15:30:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Karan7x/Movies7x.git /Movies7x
+  git clone https://github.com/otobit-sahil/moviebot.git
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Movies7x
 fi
-cd /Movies7x
+cd /moviebot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
